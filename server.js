6c3631a2c3ba68b45def59f705a22934fcebf235
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-01-30 16:43:37 -0700

    users posting to db

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+const bodyParser = require('body-parser');
 
-
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Real Time';
@@ -18,6 +21,38 @@ app.get('/', (request, response) => {
 app.get('/signin', (request, response) => {
   response.sendfile(__dirname + '/public/signin.html')
 })
+
+app.post('/users', (request,response) => {
+  const { name } = request.body;
+  console.log(request.body.name)
+  const user = {name: name}
+  database('users').insert(user)
+  .then(function() {
+    database('users').select()
+      .then(function(users){
+        response.status(200).json(users);
+      })
+      .catch(function(error) {
+        response.status(404);
+      });
+  });
+});
+
+app.post('/questions', (request,response) => {
+  const { question, user_id } = request.body;
+  console.log(user_id, question);
+  const questionInfo = {user_id:user_id, question_text:question, created_at: new Date}
+  database('questions').insert(question)
+  .then(function() {
+    database(questions).select()
+      .then(function(questions){
+        response.status(200).json(questions);
+      })
+      .catch(function(error) {
+        response.status(404);
+      });
+  });
+});
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
