cac395cb03c35bc3132a06799489220822e865b4
Nick <nicknchambers@gmail.com>
2017-01-31 18:37:20 -0700

    grabs values of input fields and makes array to submit to db

--- public/index.js
+++ public/index.js
@@ -1,13 +1,18 @@
 $('.question-form').on('submit', function(e) {
   (e).preventDefault();
   var questionText = $('#question-input').val()
-  var answerInput = $('#answer-input-1').val()
+  var inputArr = Array.from($('.answer-inputs')).map((e) => e.value)
+  console.log(inputArr)
+
+
+
+
   $.ajax({
     type: 'POST',
     url: '/questions',
     data: {
       question_text: questionText,
-      answer_text: answerInput
+      answer_text: inputArr
     }
   }).then(function(response) {
     console.log(response)
