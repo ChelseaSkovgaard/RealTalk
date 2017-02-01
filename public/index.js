0ce472ac4bf9ceb64bfe3a483b8df311a8bd516e
Nick <nicknchambers@gmail.com>
2017-02-01 14:52:33 -0700

    appends questions to root url

--- public/index.js
+++ public/index.js
@@ -1,4 +1,15 @@
-$('.question-form').on('submit', function(e) {
+$(document).ready(function(){
+  $.ajax({
+    type: 'GET',
+    url: '/questions'
+  }).then(function(response) {
+    response.forEach(function(m) {
+      $('.questions-container').append(`<p>${m.question_text}</p>`)
+    })
+  });
+})
+
+$(' .question-form').on('submit', function(e) {
   (e).preventDefault();
   var questionText = $('#question-input').val()
   var inputArr = Array.from($('.answer-inputs')).map((m) => m.value)
